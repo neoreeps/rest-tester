575ae1a7934c83e4caeb26b81e27a15923948ce6
neoreeps <kenny.speer@gmail.com>
2016-10-15 18:24:22 -0700

    Use same view controller for both header and data, simply changing the data view based on the original button pressed from main view.

--- REST Tester/AddFieldsViewController.swift
+++ REST Tester/AddFieldsViewController.swift
@@ -12,8 +12,15 @@ import UIKit
 class AddFieldsViewController: UITableViewController {
     
     weak var caller:ViewController?
+    
+    @IBOutlet weak var dataTable: UITableView!
 
     @IBOutlet var addFieldsTitle: UINavigationItem!
+    
+    // use one list for each data type, reuse the same table view but populate
+    // with different data baed on button pressed from main view
+    var headerFields = ["1", "2", "3"]
+    var dataFields = ["a", "b", "c"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +39,52 @@ class AddFieldsViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func getTableData() -> [String] {
+        if addFieldsTitle.title?.lowercased().range(of: "header") != nil {
+            return self.headerFields
+        } else {
+            return self.dataFields
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var tableData: [String]
+        
+        if addFieldsTitle.title?.lowercased().range(of: "header") != nil {
+            tableData = self.headerFields
+        } else {
+            tableData = self.dataFields
+        }
+        
+        if tableView == dataTable {
+            print("FOUND TABLE: \(tableData.count) cells")
+            return tableData.count
+        }
+        return Int()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var tableData = getTableData()
+        
+        print("TABLE: \(tableView.description)")
+        if tableView == dataTable {
+            print("FOUND VIEW!")
+            let cell = dataTable.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            
+            let row = indexPath.row
+            cell.textLabel?.text = tableData[row]
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+
     @IBAction func doneButton(_ sender: AnyObject) {
         // caller!.myTextView.text = editModeTextView.text
         
